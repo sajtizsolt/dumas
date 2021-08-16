2831227db8ea2f03ac4d6980e24afe4cd4a8792e
Sajti Zsolt Zoltán <sajtizsolt@gmail.com>
2021-08-16 22:37:03 +0200

    Wait for config file to appear

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from configreader import ConfigReader
 from dumas import Dumas
+from os import path
+import time
 import sys
 
 if __name__ == '__main__':
-  configReader = ConfigReader(sys.argv[1])
+  config_path = sys.argv[1]
+  while not path.exists(config_path):
+    time.sleep(60)
+  configReader = ConfigReader(config_path)
   configReader.read()
   config = configReader.get_config()
   Dumas(config).run(config.token)
